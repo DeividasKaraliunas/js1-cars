b8d3d12a4d49ff79f258a48c65f9fe37871f03c8
DeividasKaraliunas <deikingas@gmail.com>
2021-03-22 09:41:24 +0200

    feat: CarModel properties added

--- server/models/CarModel.js
+++ server/models/CarModel.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose')
 
-const schema = new mongoose.Schema({
-    brand: String,
-    model: String,
-    year: Number,
-    engineVolume: Number
-},
+const schema = new mongoose.Schema(
+    {
+        brand: {
+            type:String,
+            required:true
+        },
+        model:{
+            type:String,
+            required:true
+        },
+        year: {
+            type:Nubmer,
+            required:true
+        },
+        engineVolume: {
+            type:Nubmer,
+            required:true
+        }
+    },
     { timestamps: true }
 );
 
