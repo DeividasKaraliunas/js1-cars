13590c5cce543b9961076e6e9614775c22befe6d
DeividasKaraliunas <deikingas@gmail.com>
2021-03-22 09:39:19 +0200

    feat: CarModel

---
+++ server/models/CarModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    engineVolume: Number
+},
+    { timestamps: true }
+);
+
+export default mongoose.Model('Car', schema)
