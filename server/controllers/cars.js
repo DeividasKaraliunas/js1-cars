5e45f2d134c173ea7c8ea9b165382e84ffddb04d
DeividasKaraliunas <deikingas@gmail.com>
2021-03-22 10:26:01 +0200

    feat: PostCar endpoint created

--- server/controllers/cars.js
+++ server/controllers/cars.js
@@ -1,23 +1,39 @@
 const CarModel = require('../models/CarModel')
 
-const getCars = async(req, res) => {
-    try {const carsFromDB = await CarModel.find();
-    res.status(200).json({carsFromDB})
-}
-catch(error) {
-    res.status(404).json({message:error.message})
-}
-}
-
-const postCar = async(req, res) => {
-    res.send('Ateityje idesiu viena masina');
+const getCars = async (req, res) => {
+    try {
+        const carsFromDB = await CarModel.find();
+        res.status(200).json({ cars: carsFromDB })
+    }
+    catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
 
-const deleteCar = async(req, res) => {
+const postCar = async (req, res) => {
+    const { brand, model, year, engineVolume } = req.body;
+    try {
+        const newCar = await CarModel.create({ brand, model, year, engineVolume });
+        res.status(200).json({
+            car: {
+                id: newCar._id,
+                brand: newCar.brand,
+                model: newCar.model,
+                year: newCar.year,
+                engineVolume: newCar.engineVolume
+            }
+        })
+    }
+    catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+const deleteCar = async (req, res) => {
     res.send('Ateityje istrinsiu viena masina');
 }
 
-const updataCar = async(req, res) => {
+const updataCar = async (req, res) => {
     res.send('Ateityje atnaujinsiu viena masina');
 }
 
